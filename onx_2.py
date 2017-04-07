d7fe2c49515a88cba9cfc9645a10cc159b313bc0
Garin Wally <garwall101@gmail.com>
2017-04-07 11:56:43 -0600

    G's fix

--- onx_2.py
+++ onx_2.py
@@ -9,18 +9,11 @@ import arcpy
 
 fc = "C:\\z_Workspace\\Python\onxtest\\MissoulaParcelsTest\\MissoulaParcelsTest.shp"
 
-"""
 with arcpy.da.UpdateCursor(fc, ["OwnerZip"]) as cursor:
     for row in cursor:
-        if row[0] in ("0" or "0.0"):
-            row[0] = ""
+        if row[0] in ["0", "0.0"]:
+            row[0] = ""  # if NULL value desired, use None
             cursor.updateRow(row)
             # print (row)
-"""
-rows = arcpy.UpdateCursor(fc)
-for row in rows:
-    if row.OwnerZip in ("0" or "0.0"):
-        row.OwnerZip = ""
-        rows.updateRow(row)
 
 print "Processing complete"
